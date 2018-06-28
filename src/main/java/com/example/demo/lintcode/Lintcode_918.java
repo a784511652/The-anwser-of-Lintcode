a120a4c4433685b78b41c2d6aed94c95cf214e1f
jhw <784511652@qq.com>
2018-06-28 15:24:46 +0800

    嘎嘎

---
+++ src/main/java/com/example/demo/lintcode/Lintcode_918.java
@@ -0,0 +1,11 @@
+package com.example.demo.lintcode;
+
+/**
+ * @author jiahaowei
+ * @date： 2018/6/28 0028
+ * @time： 15:16
+ * @project_name： demo
+ * @Description ：
+ */
+public class Lintcode_918 {
+}
