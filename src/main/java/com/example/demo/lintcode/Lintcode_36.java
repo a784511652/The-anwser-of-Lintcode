c6fc43356a1415d8838b8219523fc3af0314cb2f
jhw <784511652@qq.com>
2018-07-01 16:08:24 +0800

    36

--- src/main/java/com/example/demo/lintcode/Lintcode_36.java
+++ src/main/java/com/example/demo/lintcode/Lintcode_36.java
@@ -12,6 +12,35 @@ package com.example.demo.lintcode;
 
 public class Lintcode_36 {
 
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        // write your code here
+        if(m>=n || head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        for (int i = 1; i < m; i++) {
+            head = head.next;
+        }
+        ListNode p_pre = head;
+        ListNode p_end = head.next;
 
+        // m<-->n 互换
+        ListNode w1 = head.next;
+        ListNode w2 = w1.next;
+        for (int i = m; i < n; i++) {
+            if(w2 == null){
+                return null;
+            }
+            ListNode temp = w2.next;
+            w2.next = w1;
+            w1 = w2;
+            w2 = temp;
+        }
+        p_pre.next = w1;
+        p_end.next = w2;
+        return dummy.next;
+    }
 
 }
